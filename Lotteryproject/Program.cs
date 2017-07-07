17bc87759ebf2c3727fa404a6144f7390c636858
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-07 14:15:35 -0400

    initial commit

---
+++ Lotteryproject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotteryproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user for lowest number in the range
+            //""for highest number in the range
+            //user sets the range 
+            //start with blank array
+            //generate numbers using loop
+
+            Console.WriteLine("Let's plays the lottery");
+
+
+
+        }
+    }
+}
